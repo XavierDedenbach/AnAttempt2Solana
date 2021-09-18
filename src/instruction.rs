741531460fc7fcc2e813207dc413f596cedc66a6
Xavier Dedenbach <dedenbach@motivo.com>
2021-09-18 16:14:34 -0700

    added instructions and integration

---
+++ src/instruction.rs
@@ -0,0 +1,12 @@
+.
+├─ src
+│  ├─ lib.rs -> registering modules
+│  ├─ entrypoint.rs -> entrypoint to the program
+│  ├─ instruction.rs -> program API, (de)serializing instruction data
+│  ├─ processor.rs -> program logic
+│  ├─ state.rs -> program objects, (de)serializing state
+│  ├─ error.rs -> program specific errors
+├─ .gitignore
+├─ Cargo.lock
+├─ Cargo.toml
+├─ Xargo.toml
